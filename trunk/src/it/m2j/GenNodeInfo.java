03b6ba7c8100003ba76361f8fa989c19a0311b2e
pepperav@gmail.com <pepperav@gmail.com@f3f33470-e4fe-9bca-c6fe-f954eed6b2e3>
2012-07-11 10:57:14 +0000

    Code Generation: Strings

---
+++ trunk/src/it/m2j/GenNodeInfo.java
@@ -0,0 +1,36 @@
+package it.m2j;
+
+public class GenNodeInfo {
+	private String name;
+	private String value;
+	private IdType type;
+	private int dim;
+	
+	public GenNodeInfo(String name, String value, IdType type, int dimension)
+	{
+		this.name = name;
+		this.value = value;
+		this.type = type;
+		this.dim = dimension;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getValue()
+	{
+		return value;
+	}
+	
+	public IdType getType()
+	{
+		return type;
+	}
+	
+	public int getDim()
+	{
+		return dim;
+	}
+}
