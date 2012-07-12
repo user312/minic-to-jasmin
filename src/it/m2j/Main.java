b3bd53e41710e009211731bf3435f2989a5354b7
Riccardo.Pulvirenti <Riccardo.Pulvirenti@gmail.com>
2012-07-12 17:08:38 +0000

    Generating code only if there are no errors

--- src/it/m2j/Main.java
+++ src/it/m2j/Main.java
@@ -17,15 +17,11 @@ import compiler.SymbolTableConstructor;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
+	private static String className = "";
+	
 	public static void main(String[] args) {
-		
-        String outfile;
-        String namepart;
-    	FileWriter fstream;
-        String className;
+		        		
+    	FileWriter fstream;        
         
 		try {
 			Symbol result;
@@ -49,38 +45,53 @@ public class Main {
 			SymbolTableConstructor stc = new SymbolTableConstructor(null);
 			root.accept(stc);
 			
-			SemanticChecker tsc = new SemanticChecker(stc.getSymbolTable(), null);
-			root.accept(tsc);		
-            
-//            // Create output file name:
-//            File f = new File(args[0]);
-//            
-//            namepart = f.getName();
-//            int fileLenght = namepart.length();
-//            
-//        	//create jasmin file
-//            if ( namepart.charAt(fileLenght - 2) == '.' && namepart.charAt(fileLenght - 1) == 'c') {
-//               className = new String(namepart.substring(0, fileLenght - 2));
-//               outfile = new String(namepart.substring(0, fileLenght - 1));
-//               outfile = outfile.concat("j");
-//            } else {
-//               className = new String(namepart);
-//               outfile = new String(namepart);
-//               outfile = outfile.concat(".j");
-//            }
-//            
-//            // Create output file: 
-//            fstream = new FileWriter(outfile);
-//            
-//            CodeGenerator cg = new CodeGenerator(stc.getSymbolTable(), null, className);
-//            root.accept(cg);
-//            
-//            fstream.write(cg.getOutput());
-//            fstream.close();
+			if (stc.getErrorCount() == 0)
+			{
+				SemanticChecker tsc = new SemanticChecker(stc.getSymbolTable(), null);
+				root.accept(tsc);
+				
+				if (tsc.getErrorCount() == 0)
+				{			
+		            // Create output file: 
+		            fstream = new FileWriter(getJasminFileName(args[0]));
+		            
+		            CodeGenerator cg = new CodeGenerator(stc.getSymbolTable(), null, className);
+		            root.accept(cg);
+		            
+		            fstream.write(cg.getOutput());
+		            fstream.close();
+				}
+				else
+					System.out.println("Compilation failed. Found " + tsc.getErrorCount() + " error(s).");
+			}
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}		
+	}	
+	
+	private static String getJasminFileName(String filePath)
+	{
+		String namepart;
+		
+		String outfile;
+        File f = new File(filePath);           
+        
+        namepart = f.getName();
+        int fileLenght = namepart.length();
+        
+    	//create jasmin file
+        if ( namepart.charAt(fileLenght - 2) == '.' && namepart.charAt(fileLenght - 1) == 'c') {
+           className = new String(namepart.substring(0, fileLenght - 2));
+           outfile = new String(namepart.substring(0, fileLenght - 1));
+           outfile = outfile.concat("j");
+        } else {
+           className = new String(namepart);
+           outfile = new String(namepart);
+           outfile = outfile.concat(".j");
+        }
+        
+		return outfile;
 	}
 
 }
