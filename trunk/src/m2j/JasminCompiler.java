d63373da601f7c003765ebb6b1214fd986cb0527
Riccardo.Pulvirenti@gmail.com <Riccardo.Pulvirenti@gmail.com@f3f33470-e4fe-9bca-c6fe-f954eed6b2e3>
2012-07-24 13:06:37 +0000

    Introducing the JasminCompiler class. Not used yet.

---
+++ trunk/src/m2j/JasminCompiler.java
@@ -0,0 +1,99 @@
+package m2j;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import java_cup.runtime.Symbol;
+
+import ast.Node;
+
+import compiler.CodeGenerator;
+import compiler.SemanticChecker;
+import compiler.SymbolTableConstructor;
+
+public class JasminCompiler {
+
+	private String fileName;
+	private String className = "";
+	
+	public JasminCompiler(String fileName)
+	{
+		this.fileName = fileName;
+	}
+		
+	public void Compile()
+	{
+		Symbol result;
+		Node root;
+    	FileWriter fstream;
+    	String jasminFile = getJasminFileName(fileName);
+    	
+    	try {
+			// Istanzio lo scanner aprendo il file d'ingresso args[0]
+			m2jLex scanner = new m2jLex(new FileReader(fileName));
+	
+			// Istanzio il parser
+			minic2jasminParser parser = new minic2jasminParser(scanner);
+	
+			//Avvio il parser
+			result = parser.parse();
+			root = (Node)result.value;
+			
+			SymbolTableConstructor stc = new SymbolTableConstructor(className, parser.registerCounter);
+			root.accept(stc);
+			
+			if (stc.getErrorCount() == 0)
+			{
+				SemanticChecker tsc = new SemanticChecker(stc.getSymbolTable());
+				root.accept(tsc);
+				
+				if (tsc.getErrorCount() == 0)
+				{			
+		            // Create output file: 
+		            fstream = new FileWriter(jasminFile);
+		            
+		            CodeGenerator cg = new CodeGenerator(stc.getSymbolTable(), className);
+		            root.accept(cg);
+		            
+		            fstream.write(cg.getOutput());
+		            fstream.close();
+				}
+				else
+					System.out.println("Compilation failed. Found " + tsc.getErrorCount() + " error(s).");
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String getJasminFileName(String filePath)
+	{
+		String namepart;
+		
+		String outfile;
+		File f = new File(filePath);           
+		
+		namepart = f.getName();
+		int fileLenght = namepart.length();
+		
+		//create jasmin file
+		if ( namepart.charAt(fileLenght - 2) == '.' && namepart.charAt(fileLenght - 1) == 'c') {
+		   className = new String(namepart.substring(0, fileLenght - 2));
+		   outfile = new String(namepart.substring(0, fileLenght - 1));
+		   outfile = outfile.concat("j");
+		} else {
+		   className = new String(namepart);
+		   outfile = new String(namepart);
+		   outfile = outfile.concat(".j");
+		}
+		
+		return outfile;
+	}
+	
+	public String getJasminFileName()
+	{
+		return getJasminFileName(this.fileName);
+	}
+}
